3caa82c92651fe98aa897ed91e38ffdc597d7abd
batzorig <batzorig.b@nomadicss.mn>
2025-07-24 11:23:20 +0800

    32323

---
+++ tz-webapp/deploy.sh
@@ -0,0 +1,8 @@
+wait
+kubectl delete -f ingress.yaml
+kubectl delete -f deployment.yaml
+kubectl delete -f service.yaml
+wait
+kubectl apply -f ingress.yaml
+kubectl apply -f deployment.yaml
+kubectl apply -f service.yaml
