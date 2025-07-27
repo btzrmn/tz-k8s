2f9de9ac05b068bfd590f16fc55c9f0e104f7b56
batzorig <batzorig.b@nomadicss.mn>
2025-07-27 16:34:17 +0800

    42

--- tz-tms/deploy.sh
+++ tz-tms/deploy.sh
@@ -1,8 +1,6 @@
 wait
-kubectl delete -f configmap.yaml
 kubectl delete -f deployment.yaml
 kubectl delete -f service.yaml
 wait
-kubectl apply -f configmap.yaml
 kubectl apply -f deployment.yaml
 kubectl apply -f service.yaml
